aabe0baa94ca1b29249f1f67f50cc2344abf31ed
gustavo gonzales <gustavogonzales@gustavos-MacBook-Pro.local>
2023-03-16 01:48:53 -0500

    fixed url https on getAll service

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -5,7 +5,7 @@ export const Post = {
     getAll : async (query : string = "") => {
         try {
 
-            const res = await fetch('http://hn.algolia.com/api/v1/search_by_date?'+query,
+            const res = await fetch('https://hn.algolia.com/api/v1/search_by_date?'+query,
                 {
                     method: 'GET'
                 });
